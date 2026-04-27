3086a329c2097429571ffa1ab0dc2724ad230ea2
Penny Yanez <pyanez@sandiego.edu>
2026-04-26 20:27:51 -0700

    Final fixup for reverseString code

--- src/main/java/StringUtils.java
+++ src/main/java/StringUtils.java
@@ -1,4 +1,19 @@
+/**Utility class for String functions.
+ *
+ */
+
 public class StringUtils {
+    /**
+     * Private constructor for public utility class.
+     */
+    private StringUtils(){
+    }
+
+    /**
+     * Reverses a word based on given string value.
+     * @param wordToReverse string that is reversed
+     * @return the reversed string
+     */
     public static String reverseString(String wordToReverse){
         return new StringBuilder(wordToReverse).reverse().toString();
     }
--- src/test/java/StringUtilsTest.java
+++ src/test/java/StringUtilsTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 
 public class StringUtilsTest {
     @Test
