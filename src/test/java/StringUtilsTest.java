4d6fec76e91f265ac675f93c0c8e52c061ee660b
Penny Yanez <pyanez@sandiego.edu>
2026-04-26 20:00:15 -0700

    Create first test fail for reverseString

--- src/test/java/StringUtilsTest.java
+++ src/test/java/StringUtilsTest.java
@@ -1,8 +1,11 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringUtilsTest {
     @Test
     public void testStringIsReversed(){
+        assertEquals("ynnep", StringUtils.reverseString());
     }
     @Test
     public void testStringIsNotReversed(){
