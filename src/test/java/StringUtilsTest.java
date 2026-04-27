93b016bcfd59aad921fcc6b128edd8dd79f8822f
Penny Yanez <pyanez@sandiego.edu>
2026-04-26 20:05:15 -0700

    Create tests for reverse string method

--- src/test/java/StringUtilsTest.java
+++ src/test/java/StringUtilsTest.java
@@ -1,19 +1,19 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class StringUtilsTest {
     @Test
     public void testStringIsReversed(){
-        assertEquals("ynnep", StringUtils.reverseString());
-    }
-    @Test
-    public void testStringIsNotReversed(){
+        assertEquals("ynnep", StringUtils.reverseString("penny"));
     }
     @Test
     public void testReverseSingleChar(){
+        assertEquals("p", StringUtils.reverseString("p"));
     }
     @Test
     public void testReverseEmptyString(){
+        assertEquals("", StringUtils.reverseString(""));
     }
 }
